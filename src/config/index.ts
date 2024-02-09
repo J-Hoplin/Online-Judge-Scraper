a4b78466759be9f5704e527886880e6cf0b83b2d
J-hoplin1 <jhoplin7259@gmail.com>
2024-02-10 00:43:11 +0900

    Feat: Add config module

---
+++ src/config/index.ts
@@ -0,0 +1,84 @@
+import chalk from 'chalk';
+import { Exclude } from 'class-transformer';
+import { Max, Min, validate } from 'class-validator';
+
+interface IScraperConfig {
+  BOJ_ROOT: string; // Root url of individual problem
+  CHUNK: number; // Reffers to maximum chunk of request of browser. Recommend to use maximum 5
+  RANGE_START: number;
+  RANGE_END: number; // End of question
+}
+
+const DefaultConfig = {
+  BOJ_ROOT: 'https://www.acmicpc.net/problem',
+  CHUNK: 3,
+  RANGE_START: 1000,
+  RANGE_END: 3000,
+};
+
+/**
+ * Encapsulate Config datas
+ *
+ * Make all validation each field and set readonly
+ *
+ * Exclude fields if illegal extraction happens
+ */
+export class ScraperConfig {
+  private static configInstance: ScraperConfig;
+
+  @Exclude() private readonly BOJ_ROOT: string;
+
+  @Exclude() @Min(1) @Max(5) private readonly CHUNK: number;
+
+  @Exclude() @Min(1000) private readonly RANGE_START: number;
+
+  @Exclude() @Max(31000) private readonly RANGE_END: number;
+
+  get baseURL() {
+    return this.BOJ_ROOT;
+  }
+
+  get chunk() {
+    return this.CHUNK;
+  }
+
+  get range() {
+    return [this.RANGE_START, this.RANGE_END];
+  }
+
+  private constructor(config: IScraperConfig = DefaultConfig) {
+    Object.assign(this, config);
+  }
+
+  public static async LoadConfig(
+    config?: IScraperConfig,
+  ): Promise<ScraperConfig> {
+    if (!ScraperConfig.configInstance) {
+      ScraperConfig.configInstance = new ScraperConfig(config);
+      // Validate Range
+      if (
+        ScraperConfig.configInstance.RANGE_START >
+        ScraperConfig.configInstance.RANGE_END
+      ) {
+        process.stdout.write(chalk.red('❌Invalid range defined') + '\n');
+        process.exit(1);
+      }
+
+      // Validate entire value
+      const validationResult = await validate(ScraperConfig.configInstance);
+      if (validationResult.length) {
+        process.stdout.write(chalk.red('❌Fail to load config datas') + '\n');
+        for (const err of validationResult) {
+          const message = `🔧${err.property}: ${Object.values(err.constraints)}`;
+          process.stdout.write(chalk.yellow(message) + '\n');
+        }
+        process.exit(1);
+      }
+      process.stdout.write(
+        chalk.green('✨Config datas successfully loaded!' + '\n'),
+      );
+    }
+
+    return ScraperConfig.configInstance;
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
 // Index.ts
+import { ScraperConfig } from './config';
+
+async function main() {
+  // Use default setting
+  const config = await ScraperConfig.LoadConfig();
+  const [start, end] = config.range;
+  for (let init = start; start < end; init += config.chunk) {}
+}
+
+main();
